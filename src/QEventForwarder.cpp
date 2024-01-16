a68f0500bcbbdbc1facf55c06ba4ce374ef9e015
onecodeWorld24 <155933653+onecodeWorld24@users.noreply.github.com>
2024-01-16 22:44:07 +0800

    Add an event forwarding class based on Qt.
    The event forwarding class uses the QMetaObject::invokeMethod method to implement the publisher's call to the subscriber interface.

---
+++ src/QEventForwarder.cpp
@@ -0,0 +1,83 @@
+﻿#include "QEventForwarder.h"
+#include <QWriteLocker>
+
+QMap<QByteArray, QList<QObject *>> QEventForwarder::m_eventsPool;
+QReadWriteLock                     QEventForwarder::m_eventLock;
+QString                            QEventForwarder::m_lastError;
+
+void QEventForwarder::unSubscribe(QObject *listener, const QByteArray &eventName)
+{
+    QWriteLocker locker(&m_eventLock);
+    int          index = -1;
+    if (m_eventsPool.contains(eventName) && (index = m_eventsPool[eventName].indexOf(listener)) >= 0
+        && index < m_eventsPool[eventName].count())
+        m_eventsPool[eventName].takeAt(index);
+}
+
+bool QEventForwarder::subscribe(QObject *listener, const QByteArray &eventName)
+{
+    QWriteLocker locker(&m_eventLock);
+    if (m_eventsPool.contains(eventName)) {
+        if (-1 != m_eventsPool[eventName].indexOf(listener)) {
+            m_lastError = QString("This object is subscribed to this eventName");
+            return false;
+        }
+        m_eventsPool[eventName].push_back(listener);
+        return true;
+    } else {
+        m_eventsPool.insert(eventName, {listener});
+        return true;
+    }
+}
+
+bool QEventForwarder::publish(const QByteArray  &eventName,
+                              Qt::ConnectionType connectionType,
+                              QGenericArgument   val0,
+                              QGenericArgument   val1,
+                              QGenericArgument   val2,
+                              QGenericArgument   val3,
+                              QGenericArgument   val4,
+                              QGenericArgument   val5,
+                              QGenericArgument   val6,
+                              QGenericArgument   val7,
+                              QGenericArgument   val8,
+                              QGenericArgument   val9)
+{
+    QReadLocker locker(&m_eventLock);
+    if (!m_eventsPool.contains(eventName)) {
+        m_lastError = QString("No objects subscribe to this eventName");
+        return false;
+    }
+    auto        methodName = methodFormatting(eventName);
+    QStringList errors;
+    auto        listeners = m_eventsPool[eventName];
+    locker.unlock();
+    for (auto listener : listeners) {
+        if (!listener)
+            continue;
+        auto ret = QMetaObject::invokeMethod(listener,
+                                             methodName,
+                                             connectionType,
+                                             val0,
+                                             val1,
+                                             val2,
+                                             val3,
+                                             val4,
+                                             val5,
+                                             val6,
+                                             val7,
+                                             val8,
+                                             val9);
+        if (!ret)
+            errors.append(QString("%1:%2")
+                              .arg(listener->metaObject()->className())
+                              .arg(listener->objectName()));
+    }
+    if (errors.isEmpty())
+        return true;
+    m_lastError = QString("%1 execution failed:[\n").arg(QString(eventName));
+    for (auto &err : errors)
+        m_lastError += QString("%1;\n").arg(err);
+    m_lastError += "]\n";
+    return false;
+}
---
+++ src/QEventForwarder.h
@@ -0,0 +1,91 @@
+﻿#pragma once
+
+#include <cxxabi.h>
+#include <memory>
+#include <QDebug>
+#include <QList>
+#include <QMap>
+#include <QObject>
+#include <QReadWriteLock>
+
+#define METHOD_PREFIX "onEvent_"
+
+class QEventForwarder : public QObject
+{
+    Q_OBJECT
+public:
+    static void unSubscribe(QObject *listener, const QByteArray &eventName);
+
+    static bool subscribe(QObject *listener, const QByteArray &eventName);
+
+    static bool publish(const QByteArray  &eventName,
+                        Qt::ConnectionType connectionType,
+                        QGenericArgument   val0 = QGenericArgument(),
+                        QGenericArgument   val1 = QGenericArgument(),
+                        QGenericArgument   val2 = QGenericArgument(),
+                        QGenericArgument   val3 = QGenericArgument(),
+                        QGenericArgument   val4 = QGenericArgument(),
+                        QGenericArgument   val5 = QGenericArgument(),
+                        QGenericArgument   val6 = QGenericArgument(),
+                        QGenericArgument   val7 = QGenericArgument(),
+                        QGenericArgument   val8 = QGenericArgument(),
+                        QGenericArgument   val9 = QGenericArgument());
+
+    static inline bool publish(const QByteArray &eventName,
+                               QGenericArgument  val0 = QGenericArgument(),
+                               QGenericArgument  val1 = QGenericArgument(),
+                               QGenericArgument  val2 = QGenericArgument(),
+                               QGenericArgument  val3 = QGenericArgument(),
+                               QGenericArgument  val4 = QGenericArgument(),
+                               QGenericArgument  val5 = QGenericArgument(),
+                               QGenericArgument  val6 = QGenericArgument(),
+                               QGenericArgument  val7 = QGenericArgument(),
+                               QGenericArgument  val8 = QGenericArgument(),
+                               QGenericArgument  val9 = QGenericArgument())
+    {
+        return publish(eventName,
+                       Qt::AutoConnection,
+                       val0,
+                       val1,
+                       val2,
+                       val3,
+                       val4,
+                       val5,
+                       val6,
+                       val7,
+                       val8,
+                       val9);
+    }
+
+    static inline QString get_Errors() { return m_lastError; }
+
+    static inline void clearEvents()
+    {
+        QWriteLocker locker(&m_eventLock);
+        m_eventsPool.clear();
+    }
+
+    static inline QByteArray methodFormatting(const QByteArray &eventName)
+    {
+        return METHOD_PREFIX + eventName;
+    }
+
+private:
+    static QMap<QByteArray, QList<QObject *>> m_eventsPool;
+
+    static QReadWriteLock m_eventLock;
+
+    static QString m_lastError;
+};
+
+
+/*
+* Suggested to use type inference:
+*   Use the third-party library https://github.com/Neargye/nameof to implement type inference, abandoning the Q_ARG macro.
+* The current version does not provide this method temporarily.
+*/
+//template<typename T>
+//QGenericArgument toArg(T &&val)
+//{
+//    return QGenericArgument(NAMEOF_TYPE_EXPR(val).data(), &val);
+//}
